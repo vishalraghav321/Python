623a6cb1cbd905518c19e7dc58d7d2f46f5fb4ec
Vishal Raghav <vishalraghav321@gmail.com>
2025-12-14 19:55:52 +0530

    Add script to count element occurrences in list
    Introduces count.py, a program that counts how many times a user-specified element appears in a predefined list.

---
+++ Lab Practicals/count.py
@@ -0,0 +1,7 @@
+# Program to count occurrence of an element
+
+lst = [1, 2, 3, 2, 4, 2, 5]
+x = int(input("Enter element to count: "))
+
+count = lst.count(x)
+print(x, "occurs", count, "time(s) in the list.")
