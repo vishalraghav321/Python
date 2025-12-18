3da9cc71cfcfd31a147b37aa53c5653a954772ec
Vishal Raghav <vishalraghav321@gmail.com>
2025-12-18 21:43:03 +0530

    Add scripts to check object memory addresses
    Added memoryCheck.py and p2.py to print the memory addresses (ids) of integer variables in Python. These scripts help explore how Python handles object identity for integers.

---
+++ memoryCheck.py
@@ -0,0 +1,14 @@
+# a=1025
+# b=1025
+# print(id(a))
+# print(id(b))
+a=24
+b=46
+print(id(a))
+print(id(b))
+# b=1025
+# print(id(a))
+# print(id(b))
+# b = 425
+# x = 1025
+# print(id(x))
---
+++ p2.py
@@ -0,0 +1,6 @@
+z = 1025
+print(id(z))
+a=24
+b=46
+print(id(a))
+print(id(b))
